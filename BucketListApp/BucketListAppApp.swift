90d21b0d0ebdb2edb87297327ec6d60eea9f27ee
Magdalena Struniawska <mag.sok@onet.pl>
2022-05-04 11:21:24 +0200

    Initial Commit

---
+++ BucketListApp/BucketListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListAppApp.swift
+//  BucketListApp
+//
+//  Created by Slawek on 04/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
